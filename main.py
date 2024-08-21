21853ca36e39f7e9480112d0f43c7502c4f31198
unknown <maheshwariarun940@gmail.comgit config --global user.name Arun>
2024-08-21 00:48:58 -0400

    Again i have created virtual envirnmont

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from textSummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from textSummarizer.logging import logger
 
 
@@ -16,12 +17,25 @@ from textSummarizer.logging import logger
 
 
 
-STAGE_NAME = "Data Validation stage"
+# STAGE_NAME = "Data Validation stage"
+
+# try:
+#     logger.info(f">>>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<")
+#     data_validation = DataValidationTrainingPipeline()
+#     data_validation.main()
+#     logger.info(f">>>>>>>>> Stage {STAGE_NAME} completed <<<<<<<\n\nx============x")
+# except Exception as e:
+#     logger.exception(e)
+#     raise e
+
+
+
+STAGE_NAME = "Data Transformation stage"
 
 try:
     logger.info(f">>>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<")
-    data_validation = DataValidationTrainingPipeline()
-    data_validation.main()
+    data_transformation = DataTransformationTrainingPipeline()
+    data_transformation.main()
     logger.info(f">>>>>>>>> Stage {STAGE_NAME} completed <<<<<<<\n\nx============x")
 except Exception as e:
     logger.exception(e)
